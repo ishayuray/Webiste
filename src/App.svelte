bcfe0c18eddf9425078146b94d27d34b0d88c760
Your Name <you@example.com>
2025-09-28 17:19:45 -0400

    Created content

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,31 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import "./app.css"
+  import {Menu, X} from '@lucide/svelte';
+  let isMenuOpen = false
 </script>
-
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<!-- Why are navbars such a pain?? -->
+<nav class="p-3 pl-10 pr-10 flex items-center justify-between">
+  <h1 class="text-xl"> 
+    <a href="/"> Ishayu Ray </a>
+  </h1>
+  <div class="flex gap-3 max-md:hidden">
+    <a href="#about"> About Me </a>
+    <a href="#contact"> Contact Me </a>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  <Menu class={(!isMenuOpen)? "min-md:hidden" : "hidden"} onclick={() => isMenuOpen = !isMenuOpen}/>
+  <X class={(isMenuOpen)? "min-md:hidden" : "hidden"} onclick={() => isMenuOpen = !isMenuOpen}/>
+</nav>
+<div class={(isMenuOpen)?"flex flex-col items-center justify-center min-md:hidden" : "hidden"}>
+  <a href="#about"> About Me </a>
+  <a href="#contact"> Contact Me </a>  
+</div>
+<article class="flex flex-col justify-center items-center text-center pl-10 pr-10 p-3 h-[90vh]" id="about">
+  <h1 class="text-4xl text-yellow-300 p-3 font-mono"> Ishayu Ray</h1>
+  <p> Hi! I'm Ishayu Ray, a high school student. </p>
+  <p> This is my website.</p>
+  <a href="#contact" class="p-2 m-3 bg-yellow-300 text-black font-semibold rounded-2xl pl-10 pr-10 hover:bg-yellow-100"> Contact Me </a>
+</article>
+<article id ="contact" class="flex flex-col justify-center items-center text-center pl-10 pr-10 p-3">
+  <h1 class="text-2xl"> Contact Me </h1>
+  <p> You can contact me at <a href="mailto:theishayuray@gmail.com" class="text-blue-400 underline">theishayuray@gmail.com</a> </p>
+</article>
